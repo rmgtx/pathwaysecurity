2e2e5c9afd93e6863e4e42dfa6ac3be9fca91e09
rmg_tx <agateyte@gmail.com>
2026-02-13 11:14:37 -0600

    fix: set base path for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'production' ? '/pathwaysecurity/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
